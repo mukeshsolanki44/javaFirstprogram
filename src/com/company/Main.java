4371919ac553ef36925f846bba7cdb56b686d7dd
mukeshsolanki44 <solankikumar667@gmail.com>
2021-03-11 23:36:21 +0500

    Hello world

---
+++ src/com/company/Main.java
@@ -0,0 +1,177 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String a = "Helo world" ;
+
+        System.out.println(a);
+
+//    =========> derect print
+
+        System.out.println("Helo world");
+
+//      =========> a++ or ++a or --a or a--
+
+//        int a = 2 ;
+//        int b = 23;
+//        System.out.println(a);
+//        System.out.println(b);
+//
+//        int c = a+ ++b;
+//
+//        System.out.println(c);
+//        System.out.println(a);
+//        System.out.println(b);
+//
+//        int a = 2 ;
+//        int b = 23;
+//        System.out.println(a);
+//        System.out.println(b);
+//
+//        int c = a++ +b;
+//
+//        System.out.println(c);
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+
+
+
+
+
+//        int a = 2 ;
+//        int b = 23;
+//        System.out.println(a);
+//        System.out.println(b);
+//
+//        int c = a-- +b;
+//
+//        System.out.println(c);
+//        System.out.println(a);
+//        System.out.println(b);
+//
+
+
+//        int a = 2 ;
+//        int b = 23;
+//        System.out.println(a);
+//        System.out.println(b);
+//
+//        int c = a + --b;
+//
+//        System.out.println(c);
+//        System.out.println(a);
+//        System.out.println(b);
+//
+
+//
+//        int a = 2 ;
+//        int b = 23;
+//        System.out.println(a);
+//        System.out.println(b);
+//
+//        int c = ++a +b;
+//
+//        System.out.println(c);
+//        System.out.println(a);
+//        System.out.println(b);
+
+
+//        int a = 2 ;
+//        int b = 23;
+//        System.out.println(a);   // 2
+//        System.out.println(b);   // 23
+//
+//        int c = ++a + ++b;
+//
+//        System.out.println(c);
+//        System.out.println(a);
+//        System.out.println(b);
+
+//        int a = 10 ;
+//        int b = 5 ;
+//        boolean c = a < b ;
+//        System.out.println(c);  // false
+
+
+//        int a = 10 ;
+//        int b = 5 ;
+//        boolean c = a > b ;
+//        System.out.println(c);   // true
+
+
+//       =========> if else condation
+
+//
+//          int a = 10 ;
+//          int b = 5 ;
+//
+//          if(a>b){
+//              System.out.println("Congrats you hack NASA ");
+//          }
+//          else {
+//              System.out.println("sorry my dear");
+//          };
+//
+//        int d = 10 ;
+//        int e = 5 ;
+//
+//        if(d<e){
+//            System.out.println("Congrats you hack NASA ");
+//        }
+//        else {
+//            System.out.println("sorry my dear");
+//        }
+
+//        for loop
+
+//        for(int i = 0 ; i <=10; i++){
+//            System.out.println(i);
+//        }
+
+//        for(int i = 0 ; i <=10; i++){
+//            System.out.println("Helo world " + i);
+//        }=========>
+//
+
+//      =========> table of two useing for loop method
+
+//        int tab = 2;
+//
+//        for (int i = 1 ; i <= 10 ; i++){
+//            String tableOfTwo = tab + "X" + i + "=" + (i * tab);
+//            System.out.println(tableOfTwo);
+//        }
+
+
+
+//        ==========> Grade formula
+
+//
+//        float optainMarks = 368;
+//        float totalMarks =500;
+//        float pers = optainMarks / totalMarks * 100;
+//
+//         char grade ;
+//        if(pers >= 90) {
+//            grade = 'A';
+//        }
+//        else if (pers >= 80 ){
+//            grade = 'B';
+//        }
+//        else if(pers >= 70){
+//            grade = 'C';
+//        }
+//        else{
+//            grade = 'F';
+//        }
+//        System.out.println("Your persentage is " +pers);
+//        System.out.println("Your Grade is "+ "'" + grade +"'");
+
+
+
+
+
+    }
+}
